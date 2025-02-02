52e9703cc57d32c0b4966d9168ccebe89a315db4
jaana <jaana.jegorova@gmail.com>
2025-02-02 23:20:43 +0200

    feat | JJ | lesson 12

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,9 +1,10 @@
 import { expect, test } from '@playwright/test'
 import { LoginDTO } from './DTO/LoginDTO'
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './DTO/OrderDto'
 
 test.describe('Login tests', async () => {
-  test('Successful authorization', async ({ request }) => {
+  test('TL-12-1 Successful authorization', async ({ request }) => {
     const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
@@ -15,7 +16,59 @@ test.describe('Login tests', async () => {
     ).toBeTruthy()
   })
 
-  test('Unsuccessful authorization using not allowed method PUT', async ({ request }) => {
+  test('TL-12-2 Successful authorization and order creation', async ({ request }) => {
+    const responseLogin = await request.post('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
+    expect(responseLogin.status()).toBe(StatusCodes.OK)
+    console.log(await responseLogin.text())
+
+    const responseCreateOrder = await request.post('https://backend.tallinn-learning.ee/orders', {
+      data: OrderDto.generateRandomOrderDto(), // data; {};
+      headers: {
+        // Content-type: plain/text
+        Authorization: 'Bearer ' + (await responseLogin.text()),
+      },
+    })
+    console.log(await responseCreateOrder.text())
+    expect(responseCreateOrder.status()).toBe(StatusCodes.OK)
+  })
+
+  test('TL-12-3 Successful authorization, order creation and order status', async ({ request }) => {
+    const responseLogin = await request.post('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
+    expect(responseLogin.status()).toBe(StatusCodes.OK)
+
+    const responseCreateOrder = await request.post('https://backend.tallinn-learning.ee/orders', {
+      data: OrderDto.generateRandomOrderDto(), // data; {};
+      headers: {
+        Authorization: 'Bearer ' + (await responseLogin.text()),
+      },
+    })
+    expect(responseCreateOrder.status()).toBe(StatusCodes.OK)
+    const createdOrder = OrderDto.serializeResponse(await responseCreateOrder.json())
+    expect(createdOrder.id).toBeDefined()
+    expect(createdOrder.id).toBeGreaterThan(0)
+
+    const responseOrderStatus = await request.get(
+      `https://backend.tallinn-learning.ee/orders/${createdOrder.id}`,
+      {
+        //const responseOrderStatus = await request.get("https://backend.tallinn-learning.ee/orders/" + createdOrder.id, {
+        headers: {
+          Authorization: 'Bearer ' + (await responseLogin.text()),
+        },
+      },
+    )
+    expect(responseOrderStatus.status()).toBe(StatusCodes.OK)
+    const requestedOrder = OrderDto.serializeResponse(await responseOrderStatus.json())
+    expect(requestedOrder.status).toBeDefined()
+    expect(requestedOrder.status).toBe('OPEN')
+  })
+
+  test.skip('TL-12-4 Unsuccessful authorization using not allowed method PUT', async ({
+    request,
+  }) => {
     const response = await request.put('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
@@ -24,7 +77,9 @@ test.describe('Login tests', async () => {
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 
-  test('Unsuccessful authorization using not allowed method DELETE', async ({ request }) => {
+  test.skip('TL-12-5 Unsuccessful authorization using not allowed method DELETE', async ({
+    request,
+  }) => {
     const response = await request.delete('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithCorrectData(),
     })
@@ -33,7 +88,9 @@ test.describe('Login tests', async () => {
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 
-  test('Unsuccessful authorization with incorrect credentials', async ({ request }) => {
+  test.skip('TL-12-6 Unsuccessful authorization with incorrect credentials', async ({
+    request,
+  }) => {
     const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
       data: LoginDTO.createLoginWithIncorrectData(),
     })
