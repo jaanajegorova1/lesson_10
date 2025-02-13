95dd39f3b79f45a3332322c6a9d3b7977f4e2151
jaana <jaana.jegorova@gmail.com>
2025-02-13 14:56:44 +0200

    feat| JJ |Homework13 + all files

--- tests/Users.EmptyArray.Api.spec.ts
+++ tests/Users.EmptyArray.Api.spec.ts
@@ -1,22 +1,9 @@
-import { expect, test } from "@playwright/test";
-import { StatusCodes } from "http-status-codes";
-import { UserDTO } from "./DTO/UserDTO";
-import { APIRequestContext } from "playwright";
+import { expect, test } from '@playwright/test'
+import { UserDTO } from './DTO/UserDTO'
 
-test.describe("User management app tests", async () => {
-  const clearUser = async (
-    id: number,
-    request: APIRequestContext,
-  ): Promise<void> => {
-    await request.delete(`http://localhost:3000/users/${id}`);
-  };
-  test("TL-14-5 get empty array for users test", async ({
-    request,
-  }): Promise<void> => {
-    const allUsersResponse = await request.get("http://localhost:3000/users");
-    const json = await allUsersResponse.json();
+test('TL-14-5 get empty array for users test', async ({ request }) => {
+  const allUsersResponse = await request.get('http://localhost:3000/users')
+  const json: UserDTO[] = await allUsersResponse.json()
 
-    expect(json.length).toBe(0);
-    expect(allUsersResponse.json).toBe(`[]`);
-  });
-});
+  expect(json.length).toBe(0)
+})
