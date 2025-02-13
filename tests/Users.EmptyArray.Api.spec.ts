2d002c40410913d17c40905d6c6116417223f12a
jaana <jaana.jegorova@gmail.com>
2025-02-13 22:08:02 +0200

    feat| JJ |TL-13-hw | Homework13_import DTO file change

--- tests/Users.EmptyArray.Api.spec.ts
+++ tests/Users.EmptyArray.Api.spec.ts
@@ -1,7 +1,6 @@
 import { expect, test } from '@playwright/test'
 import {UserDTO} from "./DTO/UserDTO";
 
-
 test('TL-14-5 get empty array for users test.', async ({ request }) => {
   const allUsersResponse = await request.get('http://localhost:3000/users')
   const json: UserDTO[] = await allUsersResponse.json()
